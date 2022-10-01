0c27c7f8758182a46b25a3074322c98a56fe9d7e
Mohammed Mudassir <mohammedmudassir687687@gmail.com>
2022-10-01 16:57:02 -0400

    CreateChannel and Sidebar coded

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Avatar, useChat, useChatContext } from 'stream-chat-react'
 
-const TeamChannelPreview = ({ channel, type }) => {
+const TeamChannelPreview = ({ setActiveChannel, channel, type, setToggleContainer, setIsCreating, setIsEditing }) => {
   const { channel: activeChannel, client } = useChatContext(); 
   
   const ChannelPreview = () => (
@@ -17,10 +17,10 @@ const TeamChannelPreview = ({ channel, type }) => {
         <div className="channel-preview__item single">
             <Avatar 
                 image={members[0]?.user?.image}
-                name={members[0]?.user?.fullName}
+                name={members[0]?.user?.fullName || members[0]?.user?.id}
                 size={24}
             />
-            <p>{members[0]?.user?.fullName}</p>
+            <p>{members[0]?.user?.fullName || members[0]?.user?.id}</p>
         </div>
     )
   }
@@ -32,7 +32,12 @@ const TeamChannelPreview = ({ channel, type }) => {
                 : 'channel-preview__wrapper'
         }
         onClick={() => {
-            console.log(channel);
+            setIsCreating(false);
+            setIsEditing(false);
+            setActiveChannel(channel);
+            if (setToggleContainer) {
+                setToggleContainer((prevState) => !prevState);
+            }
         }}
         >
             {type === 'team' ? <ChannelPreview /> : <DirectPreview />}
